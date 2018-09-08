56c22635644df18fd01a3da7d8d4cfdeb173127e
alexncode <alexnkcode@gmail.com>
2018-09-08 08:50:59 +0000

    Added sample text

--- src/components/TextArea.vue
+++ src/components/TextArea.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="text-area">
-      <textarea name="main" id="text" cols="30" rows="10" @input="$emit('textChanged', $event.target.value)"></textarea>
+      <textarea class="written-text" name="main" id="text" @input="$emit('textChanged', $event.target.value)"></textarea>
+      <div class="sample-text">
+        Some sample text
+      </div>
   </div>
 </template>
 
@@ -14,11 +17,23 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .text-area {
+  display: flex;
   width: 100%;
   height: 300px;
-  textarea {
-    height: 95%;
-    width: 95%;
+  .written-text {
+    flex: 1;
+    font-family: inherit;
+    font-size: inherit;
+    color: inherit;
+    padding: 0.5rem;
+    resize: none;
+  }
+  .sample-text {
+    text-align: left;
+    border: 1px solid gray;
+    flex: 1;
+    padding: 0.5rem;
+    margin-left: 1px;
   }
 }
 </style>
